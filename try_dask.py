ea84132b5d415e0d1bb89436764aabe876e4acc9
Matthew Rocklin <mrocklin@gmail.com>
2015-07-11 21:56:58 +0200

    separate inner from outer information

--- try_dask.py
+++ try_dask.py
@@ -15,11 +15,15 @@ from dask.compose import daskify, value
 
 D = daskify
 
-def train_test(reg_param, train_idx, test_idx, model_sel_idx, X, y):
+def train_test(reg_param, train_idx, test_idx, X, y):
     svm = SVC(C=reg_param)
-    svm.fit(X[model_sel_idx[train_idx], :], y[model_sel_idx[train_idx]])
-    return svm.score(X[model_sel_idx[test_idx], :], y[model_sel_idx[test_idx]])
+    svm.fit(X[train_idx, :], y[train_idx])
+    return svm.score(X[test_idx, :], y[test_idx])
 
+def train_test2(reg_param, train_idx, test_idx, X, y):
+    svm = SVC(C=reg_param)
+    svm.fit(X[train_idx, :], y[train_idx])
+    return svm.score(X[test_idx, :], y[test_idx])
 
 def mean(*args):
     return np.mean(args)
@@ -32,8 +36,8 @@ def sum2(*args):
 
 d = 10
 n = 100
-y = value(np.sign(np.random.randn(n)), name='u')
-X = value(np.random.randn(n, d), name='X')
+y = np.sign(np.random.randn(n))
+X = np.random.randn(n, d)
 reg_params = np.logspace(-2, 2, 5)
 n_folds = 4
 
@@ -41,13 +45,13 @@ kf_test = cross_validation.KFold(n, n_folds=n_folds)
 score_params = list()
 test_scores = []
 for model_sel_idx, test_idx in kf_test:
-    nn = len(model_sel_idx)
-    model_sel_idx = value(model_sel_idx)
-    test_idx = value(test_idx)
+    X_train = X[model_sel_idx]
+    y_train = y[model_sel_idx]
+
     for reg_param in reg_params:
         score = 0
-        kf = cross_validation.KFold(nn, n_folds=n_folds)
-        scores = [D(train_test)(reg_param, train_idx, val_idx, model_sel_idx, X, y)
+        kf = cross_validation.KFold(len(model_sel_idx), n_folds=n_folds)
+        scores = [D(train_test)(reg_param, train_idx, val_idx, X_train, y_train)
                   for train_idx, val_idx in kf]
         score = D(sum2)(*scores) / n_folds
         score_params.append((score, reg_param))
@@ -57,8 +61,7 @@ for model_sel_idx, test_idx in kf_test:
     getitem = lambda a, b: a[b]
     best_param = D(getitem)(params, best_index)
 
-    test_scores += [D(train_test)(best_param, model_sel_idx, test_idx,
-                                  np.arange(n), X, y)]
+    test_scores += [D(train_test2)(best_param, model_sel_idx, test_idx, X, y)]
 
 test_score = D(sum2)(*test_scores) / n_folds
 
