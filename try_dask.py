f3698192e56e27c62c2d95b7afccfac8a3a93448
Matthew Rocklin <mrocklin@gmail.com>
2015-07-12 23:24:11 +0200

    clean up script after adding better list handling in dask

--- try_dask.py
+++ try_dask.py
@@ -12,6 +12,7 @@ import dask
 from sklearn.svm import SVC
 from sklearn import cross_validation
 from dask.compose import daskify, value
+from dask.async import get_sync
 
 D = daskify
 
@@ -19,20 +20,6 @@ def train_test(reg_param, train_idx, test_idx, X, y):
     svm = SVC(C=reg_param)
     svm.fit(X[train_idx, :], y[train_idx])
     return svm.score(X[test_idx, :], y[test_idx])
-
-def train_test2(reg_param, train_idx, test_idx, X, y):
-    svm = SVC(C=reg_param)
-    svm.fit(X[train_idx, :], y[train_idx])
-    return svm.score(X[test_idx, :], y[test_idx])
-
-def mean(*args):
-    return np.mean(args)
-
-def argmax(*args):
-    return max(enumerate(args), key=lambda (i, x): x)[0]
-
-def sum2(*args):
-    return sum(args)
 
 d = 10
 n = 100
@@ -49,20 +36,16 @@ for model_sel_idx, test_idx in kf_test:
     y_train = y[model_sel_idx]
 
     for reg_param in reg_params:
-        score = 0
         kf = cross_validation.KFold(len(model_sel_idx), n_folds=n_folds)
         scores = [D(train_test)(reg_param, train_idx, val_idx, X_train, y_train)
                   for train_idx, val_idx in kf]
-        score = D(sum2)(*scores) / n_folds
-        score_params.append((score, reg_param))
+        score = D(sum)(scores) / n_folds
+        score_params.append([score, reg_param])
 
-    scores, params = list(zip(*score_params))
-    best_index = D(argmax)(*scores)
-    getitem = lambda a, b: a[b]
-    best_param = D(getitem)(params, best_index)
+    best_param = D(max)(score_params)[1]
 
-    test_scores += [D(train_test2)(best_param, model_sel_idx, test_idx, X, y)]
+    test_scores += [D(train_test)(best_param, model_sel_idx, test_idx, X, y)]
 
-test_score = D(sum2)(*test_scores) / n_folds
+test_score = D(sum)(test_scores) / n_folds
 
 print test_score.compute()
